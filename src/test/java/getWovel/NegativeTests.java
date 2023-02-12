e2a13482600d564a24de74d20c7d5d9fe62ecfdd
m.lungu <m.lungu@primevision.com>
2023-02-12 20:48:25 +0200

    working allure

--- src/test/java/getWovel/NegativeTests.java
+++ src/test/java/getWovel/NegativeTests.java
@@ -1,10 +1,10 @@
 package getWovel;
 
+import io.qameta.allure.Epic;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -15,50 +15,34 @@ public class NegativeTests {
         RestAssured.baseURI = "http://localhost:8080";
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "TPLKMBNV, 'TPLKMBNV', 200", // Only Consonant uppercase
-            "tplkmnv, 'tplkmnv', 200", // Only Consonant lowercase
-            "' ', ' ', 200", // One space
-            "'' , '', 200", // Empty
-            "MORE_THAN_MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
-            "LESS_THAN_MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
+//    @ParameterizedTest
+//    @CsvSource({
+//            "TPLKMBNV, 'TPLKMBNV', 200", // Only Consonant uppercase
+////            "tplkmnv, 'tplkmnv', 200", // Only Consonant lowercase
+////            "' ', ' ', 200", // One space
+////            "'' , '', 200", // Empty
+////            "MORE_THAN_MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
+////            "LESS_THAN_MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
+//
+//
+//    })
+//    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
+//        Response response = RestAssured.get("/" + givenValue);
+//
+//        response.then()
+//                .statusCode(expectedStatusCode)
+//                .body(equalTo(expectedValue));
+//    }
 
 
-    })
-    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
-        Response response = RestAssured.get("/" + givenValue);
-
+//
+    @Test
+    @Epic("Negative test with empty string")
+    public void negativeEmptyChar() {
+        Response response = RestAssured.get("/gsas");
         response.then()
-                .statusCode(expectedStatusCode)
-                .body(equalTo(expectedValue));
+                .statusCode(200)
+                .body(equalTo("ss"));
     }
-
-
-//
-//    @Test
-//    public void negativeEmptyChar() {
-//        Response response = RestAssured.get("/gsas");
-//        response.then()
-//                .statusCode(200)
-//                .body(equalTo("ss"));
-//    }
-//
-//    @Test
-//    public void negativeAboveMaxLength_200() {
-//        Response response = RestAssured.get("/gsas");
-//        response.then()
-//                .statusCode(200)
-//                .body(equalTo("ss"));
-//    }
-
-//    @Test
-//    public void negativeBelowMinLength_200() {
-//        Response response = RestAssured.get("/gsas");
-//        response.then()
-//                .statusCode(200)
-//                .body(equalTo("ss"));
-//    }
-
 
 }
--- src/test/java/getWovel/PositiveTests.java
+++ src/test/java/getWovel/PositiveTests.java
@@ -1,39 +1,39 @@
-package getWovel;
-
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.hamcrest.Matchers.equalTo;
-
-public class PositiveTests {
-
-    @BeforeAll
-    public static void setUp() {
-        RestAssured.baseURI = "http://localhost:8080";
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "aeiou, '', 200", // Only Vowels lowercase
-            "AEIOU, '', 200", // Only Vowels uppercase
-            "sasepisolusasepisolu,sspslsspsl, 200",  // Only lowercase
-            "SASEPISOLUSASEPISOLU, SSPSLSSPSL, 200", // Only uppercase
-            "sasepisolu sasepisolu,sspsl sspsl, 200",  // Only lowercase with space
-            "SAsasepisolu EPIsasepisoluOLU, Ssspsl PsspslL, 200", // Combined lowercase and uppercase
-            "@#%$aei@#o$32u,@#%$@#o$u, 200",  // TBD Only lowercase
-//            "MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
-//            "MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
-
-    })
-    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
-        Response response = RestAssured.get("/" + givenValue);
-
-        response.then()
-                .statusCode(expectedStatusCode)
-                .body(equalTo(expectedValue));
-    }
-}
+//package getWovel;
+//
+//import io.restassured.RestAssured;
+//import io.restassured.response.Response;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvSource;
+//
+//import static org.hamcrest.Matchers.equalTo;
+//
+//public class PositiveTests {
+//
+//    @BeforeAll
+//    public static void setUp() {
+//        RestAssured.baseURI = "http://localhost:8080";
+//    }
+//
+//    @ParameterizedTest
+//    @CsvSource({
+//            "aeiou, '', 200", // Only Vowels lowercase
+//            "AEIOU, '', 200", // Only Vowels uppercase
+//            "sasepisolusasepisolu,sspslsspsl, 200",  // Only lowercase
+//            "SASEPISOLUSASEPISOLU, SSPSLSSPSL, 200", // Only uppercase
+//            "sasepisolu sasepisolu,sspsl sspsl, 200",  // Only lowercase with space
+//            "SAsasepisolu EPIsasepisoluOLU, Ssspsl PsspslL, 200", // Combined lowercase and uppercase
+//            "@#%$aei@#o$32u,@#%$@#o$u, 200",  // TBD Only lowercase
+////            "MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
+////            "MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
+//
+//    })
+//    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
+//        Response response = RestAssured.get("/" + givenValue);
+//
+//        response.then()
+//                .statusCode(expectedStatusCode)
+//                .body(equalTo(expectedValue));
+//    }
+//}
