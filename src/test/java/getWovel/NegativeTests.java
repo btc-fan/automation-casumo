0663bb259f415718e8c406d62e7cc1d90b694cb9
m.lungu <m.lungu@primevision.com>
2023-02-27 19:45:35 +0200

    update latest version of junit5 and allure. Add allure.properties file

--- src/test/java/getWovel/NegativeTests.java
+++ src/test/java/getWovel/NegativeTests.java
@@ -3,6 +3,7 @@ package getWovel;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,34 +16,34 @@ public class NegativeTests {
         RestAssured.baseURI = "http://localhost:8080";
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "TPLKMBNV, 'TPLKMBNV', 200", // Only Consonant uppercase
-            "tplkmnv, 'tplkmnv', 200", // Only Consonant lowercase
-            "' ', ' ', 200", // One space
-            "'' , '', 200", // Empty
-            "MORE_THAN_MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
-            "LESS_THAN_MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
-
-
-    })
-    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
-        Response response = RestAssured.get("/" + givenValue);
-
-        response.then()
-                .statusCode(expectedStatusCode)
-                .body(equalTo(expectedValue));
-    }
-
-
+//    @ParameterizedTest
+//    @CsvSource({
+//            "TPLKMBNV, 'TPLKMBNV', 200", // Only Consonant uppercase
+//            "tplkmnv, 'tplkmnv', 200", // Only Consonant lowercase
+//            "' ', ' ', 200", // One space
+//            "'' , '', 200", // Empty
+//            "MORE_THAN_MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
+//            "LESS_THAN_MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
 //
-//    @Test
-//    public void negativeEmptyChar() {
-//        Response response = RestAssured.get("/gsas");
+//
+//    })
+//    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
+//        Response response = RestAssured.get("/" + givenValue);
+//
 //        response.then()
-//                .statusCode(200)
-//                .body(equalTo("ss"));
+//                .statusCode(expectedStatusCode)
+//                .body(equalTo(expectedValue));
 //    }
+
+
+
+    @Test
+    public void negativeEmptyChar() {
+        Response response = RestAssured.get("/gsas");
+        response.then()
+                .statusCode(200)
+                .body(equalTo("gss"));
+    }
 //
 //    @Test
 //    public void negativeAboveMaxLength_200() {
--- src/test/java/getWovel/PositiveTests.java
+++ src/test/java/getWovel/PositiveTests.java
@@ -16,24 +16,24 @@ public class PositiveTests {
         RestAssured.baseURI = "http://localhost:8080";
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "aeiou, '', 200", // Only Vowels lowercase
-            "AEIOU, '', 200", // Only Vowels uppercase
-            "sasepisolusasepisolu,sspslsspsl, 200",  // Only lowercase
-            "SASEPISOLUSASEPISOLU, SSPSLSSPSL, 200", // Only uppercase
-            "sasepisolu sasepisolu,sspsl sspsl, 200",  // Only lowercase with space
-            "SAsasepisolu EPIsasepisoluOLU, Ssspsl PsspslL, 200", // Combined lowercase and uppercase
-            "@#%$aei@#o$32u,@#%$@#o$u, 200",  // TBD Only lowercase
-//            "MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
-//            "MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
-
-    })
-    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
-        Response response = RestAssured.get("/" + givenValue);
-
-        response.then()
-                .statusCode(expectedStatusCode)
-                .body(equalTo(expectedValue));
-    }
+//    @ParameterizedTest
+//    @CsvSource({
+//            "aeiou, '', 200", // Only Vowels lowercase
+//            "AEIOU, '', 200", // Only Vowels uppercase
+//            "sasepisolusasepisolu,sspslsspsl, 200",  // Only lowercase
+//            "SASEPISOLUSASEPISOLU, SSPSLSSPSL, 200", // Only uppercase
+//            "sasepisolu sasepisolu,sspsl sspsl, 200",  // Only lowercase with space
+//            "SAsasepisolu EPIsasepisoluOLU, Ssspsl PsspslL, 200", // Combined lowercase and uppercase
+//            "@#%$aei@#o$32u,@#%$@#o$u, 200",  // TBD Only lowercase
+////            "MAX_LENGTH , 'TBD', 200", // TODO find out what is max length
+////            "MIN_LENGTH , 'TBD', 200", // TODO find out what is min length
+//
+//    })
+//    public void testGetWithPositiveValues(String givenValue, String expectedValue, int expectedStatusCode) {
+//        Response response = RestAssured.get("/" + givenValue);
+//
+//        response.then()
+//                .statusCode(expectedStatusCode)
+//                .body(equalTo(expectedValue));
+//    }
 }
